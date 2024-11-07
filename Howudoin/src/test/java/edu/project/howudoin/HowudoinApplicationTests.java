5920dee88088d75002ccc3a65df314b513d0946a
Furkyleon <furkan.yilmaz@sabanciuniv.edu>
2024-11-07 14:56:58 +0300

    project uploaded

---
+++ Howudoin/src/test/java/edu/project/howudoin/HowudoinApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.project.howudoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowudoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
